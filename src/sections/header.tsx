e49529e2456918efeff07620d54873ea95a92a33
thomasschafer <thomasschafer97@gmail.com>
2021-10-10 21:51:03 +0100

    feat: updated main menu animation

--- src/sections/header.tsx
+++ src/sections/header.tsx
@@ -28,7 +28,40 @@ export const HeaderSection = ({
           className="show-main-menu-link mobile-only"
           onClick={() => toggleMainMenu(showMainMenu)}
         >
-          <p>SHOW MAIN MENU</p>
+          <svg
+            width="150"
+            height="150"
+            viewBox="0 0 150 150"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            id="main-menu-icon"
+            className={showMainMenu ? "close-menu" : "open-menu"}
+          >
+            <path
+              id="background-circle"
+              d="M150 75C150 116.421 116.421 150 75 150C33.5786 150 0 116.421 0 75C0 33.5786 33.5786 0 75 0C116.421 0 150 33.5786 150 75Z"
+              fill="white"
+            />
+            <line
+              id="bottom-line"
+              x1="30"
+              y1="105"
+              x2="120"
+              y2="105"
+              stroke="black"
+              strokeWidth="10"
+            />
+            <line
+              id="middle-line"
+              x1="30"
+              y1="75"
+              x2="120"
+              y2="75"
+              stroke="black"
+              strokeWidth="10"
+            />
+            <line id="top-line" x1="30" y1="45" x2="120" y2="45" stroke="black" strokeWidth="10" />
+          </svg>
         </div>
       </div>
     </div>
